df40f6ee824a128787be8f77033e737184d24ba8
eugeneskom <eugeneskom@gmail.com>
2022-10-18 22:21:54 +0300

    first commit

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,15 +1,15 @@
-var swiper = new Swiper(".mySwiper", {
- slidesPerView: 3,
- spaceBetween: 30,
- slidesPerGroup: 3,
+var swiper = new Swiper(".spoon__slider", {
+ slidesPerView: 1,
  loop: true,
+ // spaceBetween: 30,
+ // slidesPerGroup: 3,
  // loopFillGroupWithBlank: true,
- pagination: {
-   el: ".swiper-pagination",
-   clickable: true,
- },
- navigation: {
-   nextEl: ".swiper-button-next",
-   prevEl: ".swiper-button-prev",
- },
+ // pagination: {
+ //   el: ".swiper-pagination",
+ //   clickable: true,
+ // },
+ // navigation: {
+ //   nextEl: ".swiper-button-next",
+ //   prevEl: ".swiper-button-prev",
+ // },
 });
